c07dd59b81bfd359a355a455034486494728493b
Kate test <d.swetlov2017@gmail.com>
2023-10-06 13:18:11 +0300

    initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,68 @@
+package ru.netology.stats;
+
+
+public class StatsService {
+    public int calculateSumSales(int[] sales) {
+        int sum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sum += sales[i];
+        }
+        return sum;
+    }
+
+    public int getMinSales(int[] sales) {
+        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
+                minMonth = i; // запомним его как минимальный
+            }
+        }
+
+        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int getMaxSales(int[] sales) {
+        int maxMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth + 1;
+    }
+
+    public int getAverageSales(int[] sales) {
+        int averageSales = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= 0) {
+                averageSales = calculateSumSales(sales) / sales.length;
+            }
+        }
+        return averageSales;
+    }
+
+    public int getHigherAverageSales(int[] sales) {
+        int higherAverage = 0;
+
+        for (int i = 0; i > sales.length; i++) {
+            if (sales[i] > getAverageSales(sales)) {
+                higherAverage = i;
+            }
+        }
+        return higherAverage + 1;
+    }
+
+    public int getBelowAverageSales(int[] sales) {
+        int belowAverage = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < getAverageSales(sales)) {
+                belowAverage = i;
+            }
+        }
+        return belowAverage + 1;
+    }
+}
+
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,63 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void calculateSumSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+        int actual = service.calculateSumSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void shouldFindMin() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.getMinSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMax() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.getMaxSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindAverage() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        int actual = service.getAverageSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindHigherAverageSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 1;
+        int actual = service.getHigherAverageSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindBelowAverageSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 11;
+        int actual = service.getBelowAverageSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
+
